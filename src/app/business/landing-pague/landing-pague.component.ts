4cb182d9fc7a4e36f1a4362fdd27ba5f2de7314d
Santiago <ardyosa@hotmail.com>
2024-07-14 05:32:33 -0500

    Se agrega componente Login, componente landingpage, se incluse el mismo color en una nav bar, se organiza el router con la estructura nueva de angular vista en clase, se incluye boton descargar libro e inicio en la sidebar, se homologa la letra de la side bar y la header, se organizan los links del footer, se organiza el responsive del header y se organiza para que pueda ser incluido en cualquier componente y no se tenga que realizar configuraciones mayores. se decarga un svg para el link de descargar libro.

---
+++ src/app/business/landing-pague/landing-pague.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../../shared/components/header/header.component';
+
+@Component({
+  selector: 'app-landing-pague',
+  standalone: true,
+  imports: [HeaderComponent],
+  templateUrl: './landing-pague.component.html',
+  styleUrl: './landing-pague.component.css'
+})
+export class LandingPagueComponent {
+
+}

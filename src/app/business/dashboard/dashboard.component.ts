d5948c4a1f84f977b4776026d4b8d1a452ea8424
mauro258 <mejiamauricio1009@gmail.com>
2024-07-16 09:02:57 -0500

    se agrega libreria para utilizar graficas y se actualizan algunos detalles esteticos en el dashboard

--- src/app/business/dashboard/dashboard.component.ts
+++ src/app/business/dashboard/dashboard.component.ts
@@ -1,12 +1,56 @@
 import { Component } from '@angular/core';
+import {NgxChartsModule} from '@swimlane/ngx-charts'
+
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [NgxChartsModule],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrl: './dashboard.component.css',
 })
 export default class DashboardComponent {
+  view: [number,number] = [700, 400];
 
+  // options
+  gradient: boolean = true;
+  showLegend: boolean = true;
+  showLabels: boolean = true;
+  isDoughnut: boolean = false;
+
+  colorScheme = {
+    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA'],
+  };
+
+  single = [
+    {
+      name: 'Todays Users',
+      value: 2300,
+    },
+    {
+      name: 'New Clients',
+      value: 3462,
+    },
+    {
+      name: 'Sales',
+      value: 103430,
+    },
+    
+  ];
+
+  constructor() {
+    //Object.assign(this, { single });
+  }
+
+  onSelect(data: any): void {
+    console.log('Item clicked', JSON.parse(JSON.stringify(data)));
+  }
+
+  onActivate(data: any): void {
+    console.log('Activate', JSON.parse(JSON.stringify(data)));
+  }
+
+  onDeactivate(data: any): void {
+    console.log('Deactivate', JSON.parse(JSON.stringify(data)));
+  }
 }

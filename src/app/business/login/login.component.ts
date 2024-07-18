0a77157fc486ac7d68974e4b76a54a8470b037f3
Santiago <ardyosa@hotmail.com>
2024-07-17 19:31:41 -0500

    Ajuste footer absolute botton

--- src/app/business/login/login.component.ts
+++ src/app/business/login/login.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../shared/components/header/header.component';
+import { FooterComponent } from '../../shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent,FooterComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })

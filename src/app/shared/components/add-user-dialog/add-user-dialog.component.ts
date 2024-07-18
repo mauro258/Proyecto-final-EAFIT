a793a74bfcf45137106d3706d1d55cb364c0d7f6
mauro258 <mejiamauricio1009@gmail.com>
2024-07-18 12:22:56 -0500

    se aplican las funcionalidades a la tabla de perfiles para poder agregar,actualizar y eliminar usuarios

--- src/app/shared/components/add-user-dialog/add-user-dialog.component.ts
+++ src/app/shared/components/add-user-dialog/add-user-dialog.component.ts
@@ -1,15 +1,49 @@
-import { Component } from '@angular/core';
-import { MatDialogRef } from '@angular/material/dialog';
+import { Component, Inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatDialogRef, MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-user-dialog',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatDialogModule
+  ],
   templateUrl: './add-user-dialog.component.html',
-  styleUrl: './add-user-dialog.component.css',
+  styleUrls: ['./add-user-dialog.component.css']
 })
-export class AddUserDialogComponent {
-  constructor(public dialogRef: MatDialogRef<AddUserDialogComponent>) {}
+export class AddUserDialogComponent implements OnInit {
+  userForm: FormGroup;
+  isEditMode: boolean;
+
+  constructor(
+    public dialogRef: MatDialogRef<AddUserDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private fb: FormBuilder
+  ) {
+    this.isEditMode = !!data;
+    this.userForm = this.fb.group({
+      id: [data?.id || null],
+      nombre: [data?.nombre || '', Validators.required],
+      roles: [data?.roles || '', Validators.required],
+      estado: [data?.estado || '', Validators.required], // Cambiado a string vacío por defecto
+      vencimiento: [data?.vencimiento || '', Validators.required]
+    });
+  }
+
+  ngOnInit() {
+    if (this.isEditMode) {
+      this.userForm.patchValue(this.data);
+    }
+  }
+
+  onSubmit() {
+    if (this.userForm.valid) {
+      this.dialogRef.close(this.userForm.value);
+    }
+  }
 
   onCancel(): void {
     this.dialogRef.close();

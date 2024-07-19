008c59eb3207a7cacee246093a10ca3e2f72c3af
Santiago <ardyosa@hotmail.com>
2024-07-19 17:07:23 -0500

    Login y landing page finalizada

--- src/app/shared/components/register/register.component.ts
+++ src/app/shared/components/register/register.component.ts
@@ -14,6 +14,7 @@ import { HeaderComponent } from '../header/header.component';
 })
 
 export class RegisterComponent {
+  
   constructor(private router: Router) {}
 
   registerForm = new FormGroup({

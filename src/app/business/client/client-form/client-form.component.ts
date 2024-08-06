943b4d04d11830505746059885ee41fa4f300263
Verónica Sierra <vssombre@outlook.com>
2024-08-06 15:08:55 -0500

    Se crea el component de client-header, se crea el layout del cliente y se configura los  hijos cliente-portal y client-form, se crea el menú desplegable del cliente con su respectivo comportamiento, se cambia el tamaño en que la barra lateral y el menu sincronizadamente aparecen y desaparecen.

--- src/app/business/client/client-form/client-form.component.ts
+++ src/app/business/client/client-form/client-form.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { FooterComponent } from '../../../shared/components/footer/footer.component';
 import { HeaderComponent } from '../../../shared/components/header/header.component';
 import { ClientSidebarComponent } from '../client-sidebar/client-sidebar.component';
+import { ClientHeaderComponent } from "../client-header/client-header.component";
 
 @Component({
   selector: 'app-client-form',
   standalone: true,
-  imports: [ClientSidebarComponent, HeaderComponent, FooterComponent],
+  imports: [ClientSidebarComponent, HeaderComponent, FooterComponent, ClientHeaderComponent],
   templateUrl: './client-form.component.html',
   styleUrl: './client-form.component.css'
 })
